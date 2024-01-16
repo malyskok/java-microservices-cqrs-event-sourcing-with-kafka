5293f931cb5c6c41fa12dfb023ee2dbfcb7d9950
malyskok <kamil.malysko@gmail.com>
2024-01-16 16:12:34 +0100

    pojos for commands and events

---
+++ cqrs-es/cqrs.core/src/main/java/com/techbank/cqrs/core/events/BaseEvent.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2024. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+package com.techbank.cqrs.core.events;
+
+import com.techbank.cqrs.core.messages.Message;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class BaseEvent extends Message {
+
+    private int version;
+}
