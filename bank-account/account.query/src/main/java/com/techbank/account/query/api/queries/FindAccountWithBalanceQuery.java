3d6ef031eddd32ed34b9e69c0fe8ec8b59faaba4
malyskok <kamil.malysko@gmail.com>
2024-01-21 12:16:30 +0100

    Queries and Queries Dispatching

---
+++ bank-account/account.query/src/main/java/com/techbank/account/query/api/queries/FindAccountWithBalanceQuery.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2024. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+package com.techbank.account.query.api.queries;
+
+import com.techbank.account.query.api.dto.EqualityType;
+import com.techbank.cqrs.core.queries.BaseQuery;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class FindAccountWithBalanceQuery extends BaseQuery {
+    private EqualityType equalityType;
+    private double balance;
+}
---
+++ cqrs-es/cqrs.core/src/main/java/com/techbank/cqrs/core/infrastructure/QueryDispatcher.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2024. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+package com.techbank.cqrs.core.infrastructure;
+
+import com.techbank.cqrs.core.domain.BaseEntity;
+import com.techbank.cqrs.core.queries.BaseQuery;
+import com.techbank.cqrs.core.queries.QueryHandlerMethod;
+
+import java.util.List;
+
+public interface QueryDispatcher {
+    <T extends BaseQuery> void registerHandler(Class<T> type, QueryHandlerMethod<T> handler);
+    <U extends BaseEntity> List<U> send(BaseQuery query);
+}
