f5fe09db1a6b57ac481bdf7be470ec60ebb21b3e
malyskok <kamil.malysko@gmail.com>
2024-01-21 11:34:24 +0100

    Deposit, withdraw and close Account REST controllers

---
+++ bank-account/account.cmd/src/main/java/com/techbank/account/cmd/api/controllers/DepositFundsController.java
@@ -0,0 +1,39 @@
+package com.techbank.account.cmd.api.controllers;
+
+import com.techbank.account.cmd.api.commands.DepositFundsCommand;
+import com.techbank.account.common.dto.BaseResponse;
+import com.techbank.cqrs.core.exception.AggregateNotFoundException;
+import com.techbank.cqrs.core.infrastructure.CommandDispatcher;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.MessageFormat;
+
+@Slf4j
+@RestController
+@RequestMapping(path = "/api/v1/depositFunds")
+public class DepositFundsController {
+
+    @Autowired
+    private CommandDispatcher commandDispatcher;
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<BaseResponse> depositFunds(@PathVariable(value = "id") String id,
+                                                     @RequestBody DepositFundsCommand command) {
+        try {
+            command.setId(id);
+            commandDispatcher.send(command);
+            return new ResponseEntity<>(new BaseResponse("Deposit funds request completed successfully!"), HttpStatus.OK);
+        } catch (IllegalStateException | AggregateNotFoundException e) {
+            log.warn(MessageFormat.format("Client made a bad request - {0}.", e.toString()));
+            return new ResponseEntity<>(new BaseResponse(e.toString()), HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            var safeErrorMessage = MessageFormat.format("Error while processing request to deposit funds to bank account with id - {0}.", id);
+            log.error(safeErrorMessage, e);
+            return new ResponseEntity<>(new BaseResponse(safeErrorMessage), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
---
+++ bank-account/account.cmd/src/main/java/com/techbank/account/cmd/api/controllers/WithdrawFundsController.java
@@ -0,0 +1,39 @@
+package com.techbank.account.cmd.api.controllers;
+
+import com.techbank.account.cmd.api.commands.WithdrawFundsCommand;
+import com.techbank.account.common.dto.BaseResponse;
+import com.techbank.cqrs.core.exception.AggregateNotFoundException;
+import com.techbank.cqrs.core.infrastructure.CommandDispatcher;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.MessageFormat;
+
+@Slf4j
+@RestController
+@RequestMapping(path = "/api/v1/withdrawFunds")
+public class WithdrawFundsController {
+
+    @Autowired
+    private CommandDispatcher commandDispatcher;
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<BaseResponse> withdrawFunds(@PathVariable(value = "id") String id,
+                                                     @RequestBody WithdrawFundsCommand command) {
+        try {
+            command.setId(id);
+            commandDispatcher.send(command);
+            return new ResponseEntity<>(new BaseResponse("Withdraw funds request completed successfully!"), HttpStatus.OK);
+        } catch (IllegalStateException | AggregateNotFoundException e) {
+            log.warn(MessageFormat.format("Client made a bad request - {0}.", e.toString()));
+            return new ResponseEntity<>(new BaseResponse(e.toString()), HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            var safeErrorMessage = MessageFormat.format("Error while processing request to withdraw funds from bank account with id - {0}.", id);
+            log.error(safeErrorMessage, e);
+            return new ResponseEntity<>(new BaseResponse(safeErrorMessage), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
